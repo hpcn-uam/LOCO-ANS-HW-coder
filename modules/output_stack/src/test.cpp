3349aba7aecbd58000b760e37e7691e9d0c82c9b
tobi <tobias.alonso@estudiante.uam.es>
2021-05-25 16:15:34 +0200

    [output_stack] write_binary_stack and  read_binary_stack with input bytes as template param

--- modules/output_stack/src/test.cpp
+++ modules/output_stack/src/test.cpp
@@ -14,22 +14,22 @@ using namespace hls;
 // #define DEBUG 1
 
 void output_stack_sw(
-  stream<byte_block > &in, 
-  stream<byte_block > &out,
+  stream<byte_block<OUT_WORD_BYTES> > &in, 
+  stream<byte_block<OUT_WORD_BYTES> > &out,
   ap_uint<1> &golden_stack_overflow){
-  std::vector<byte_block> aux_vector;
-  byte_block in_byte_block;
+  std::vector<byte_block<OUT_WORD_BYTES>> aux_vector;
+  byte_block<OUT_WORD_BYTES> in_byte_block;
   bool first_read = true;
   do{
     in_byte_block = in.read();
     auto out_byte_block = in_byte_block;
-    out_byte_block.last_block = first_read? 1:0;
+    out_byte_block.set_last(first_read);
     aux_vector.push_back(out_byte_block);
     first_read = false;
-  }while(in_byte_block.last_block == 0);
+  }while(!in_byte_block.is_last());
 
   while (!aux_vector.empty()){
-    byte_block out_byte_block = aux_vector.back();
+    byte_block<OUT_WORD_BYTES> out_byte_block = aux_vector.back();
     aux_vector.pop_back();
     out << out_byte_block;
   }
@@ -49,21 +49,21 @@ int main(int argc, char const *argv[])
   }
 
   for (int blk_idx = 0; blk_idx < NUM_OF_BLCKS; ++blk_idx){
-    stream<byte_block> in_hw_data,in_sw_data;
+    stream<byte_block<OUT_WORD_BYTES>> in_hw_data,in_sw_data;
     cout<<"Processing block "<<blk_idx;
     int block_size = OUTPUT_STACK_SIZE +10 - int(blk_idx/2)*5;
     
     //generate data
     for (int i = 0; i < block_size; ++i){
-      byte_block in_elem;
+      byte_block<OUT_WORD_BYTES> in_elem;
       in_elem.data = i;
-      in_elem.bytes = (i == block_size-1)? (i%OUT_WORD_BYTES)+1 : OUT_WORD_BYTES ;
-      in_elem.last_block = (i == block_size-1)?1:0 ;
+      in_elem.set_num_of_bytes((i == block_size-1)? (i%OUT_WORD_BYTES)+1 : OUT_WORD_BYTES );
+      in_elem.set_last(i == block_size-1);
       in_hw_data << in_elem;
       in_sw_data << in_elem;
     }
 
-    stream<byte_block> out_hw_data,out_sw_data;
+    stream<byte_block<OUT_WORD_BYTES>> out_hw_data,out_sw_data;
     ap_uint<1> stack_overflow, golden_stack_overflow;
     output_stack(in_hw_data,out_hw_data,stack_overflow);
     output_stack_sw(in_sw_data,out_sw_data,golden_stack_overflow);
@@ -77,10 +77,10 @@ int main(int argc, char const *argv[])
 
     if(golden_stack_overflow ==1) {
       while(!out_hw_data.empty()) {
-        byte_block out_hw_elem = out_hw_data.read();
+        byte_block<OUT_WORD_BYTES> out_hw_elem = out_hw_data.read();
       }
       while(!out_sw_data.empty()) {
-        byte_block out_sw_elem = out_sw_data.read();
+        byte_block<OUT_WORD_BYTES> out_sw_elem = out_sw_data.read();
       }
 
       cout<<"| SUCCESS (Overflow correctly detected, data ignored)"<<endl;
@@ -91,11 +91,11 @@ int main(int argc, char const *argv[])
 
     int i = 0;
     while(!out_hw_data.empty()) {
-      byte_block out_hw_elem = out_hw_data.read();
-      byte_block out_sw_elem = out_sw_data.read();
+      byte_block<OUT_WORD_BYTES> out_hw_elem = out_hw_data.read();
+      byte_block<OUT_WORD_BYTES> out_sw_elem = out_sw_data.read();
       ASSERT(out_hw_elem.data,==,out_sw_elem.data,"Blk "<<blk_idx <<" | i:"<<i)
-      ASSERT(out_hw_elem.bytes,==,out_sw_elem.bytes,"Blk "<<blk_idx <<" | i:"<<i)
-      ASSERT(out_hw_elem.last_block,==,out_sw_elem.last_block,"Blk "<<blk_idx <<" | i:"<<i)
+      ASSERT(out_hw_elem.num_of_bytes(),==,out_sw_elem.num_of_bytes(),"Blk "<<blk_idx <<" | i:"<<i)
+      ASSERT(out_hw_elem.is_last(),==,out_sw_elem.is_last(),"Blk "<<blk_idx <<" | i:"<<i)
 
       i++;
     }
