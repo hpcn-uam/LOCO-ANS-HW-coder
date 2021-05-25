3b8704e3c389d3177fab31fc5cd710da69dabae0
tobi <tobias.alonso@estudiante.uam.es>
2021-05-25 16:17:18 +0200

    [pack_out_bytes] function is able to pack bytes from IB to OB byte words where IB <= OB. Passing all tests

--- modules/pack_out_bytes/script.tcl
+++ modules/pack_out_bytes/script.tcl
@@ -1,5 +1,5 @@
 open_project pack_out_bytes.hls_prj
-set_top pack_out_bytes
+set_top pack_out_bytes_top
 add_files src/pack_out_bytes.cpp -cflags "-DPACK_OUT_BYTES_TOP"
 add_files -tb src/test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "solution1" -flow_target vivado
@@ -9,4 +9,4 @@ create_clock -period 10 -name default
 csim_design -clean
 csynth_design
 cosim_design -enable_dataflow_profiling -trace_level all
-# export_design -flow syn -format ip_catalog
+export_design -flow syn -format ip_catalog
