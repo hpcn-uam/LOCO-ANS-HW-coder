2f48f3ead1a16ae6b853a17ff5aa505b037fab15
tobi <tobias.alonso@estudiante.uam.es>
2021-05-25 16:18:09 +0200

    [TSG_coder] Integration of module changes

--- script.tcl
+++ script.tcl
@@ -13,4 +13,4 @@ create_clock -period 10 -name default
 csim_design -clean
 csynth_design
 cosim_design -enable_dataflow_profiling -trace_level all
-export_design -flow syn -format ip_catalog
+# export_design -flow syn -format ip_catalog
