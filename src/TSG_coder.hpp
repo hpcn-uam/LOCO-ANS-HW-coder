4b691089fe6cacbd9d2e105e646a2bbcfed422c6
tobi <tobias.alonso@estudiante.uam.es>
2021-07-05 10:37:09 +0200

    [TSG coder] Integrating output stask changes. csim and hls synth passing (hls synth reports not checked). Cosim failing (seems to be an HLS bug)

--- src/TSG_coder.hpp
+++ src/TSG_coder.hpp
@@ -2,7 +2,9 @@
 #ifndef TSG_CODER_HPP
 #define TSG_CODER_HPP
 
+
 #include <hls_stream.h>
+#include <ap_axi_sdata.h>
 #include <tgmath.h>
 #include "../modules/coder_config.hpp"
 #include "../modules/input_buffers/src/input_buffers.hpp"
@@ -11,7 +13,18 @@
 #include "../modules/output_stack/src/output_stack.hpp"
 #include "../modules/pack_out_bytes/src/pack_out_bytes.hpp"
 
+
+typedef axis<ap_uint<OUTPUT_SIZE>,OUTPUT_STACK_BYTES_SIZE,0,0 > TSG_out_intf;
+
+
 void TSG_coder(
+  //input
   stream<coder_interf_t> &in,
-  stream<byte_block<OUT_DMA_BYTES>> &byte_block_stream);
+  //output
+  stream<byte_block<OUT_WORD_BYTES>> &byte_block_stream,
+  stream<ap_uint<OUTPUT_STACK_BYTES_SIZE> > &last_byte_idx
+  //status registers
+  // ap_uint<1> stack_overflow
+  );
+
 #endif // TSG_CODER_HPP
