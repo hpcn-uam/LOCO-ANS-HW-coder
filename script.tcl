622358341c80ed7039ad31b7ec027699691e0853
tobi <tobias.alonso@estudiante.uam.es>
2021-07-06 18:58:06 +0200

    [TSG coder] add argument to script to avoid csim and RTL cosim

--- script.tcl
+++ script.tcl
@@ -1,3 +1,20 @@
+
+# For arg parsing to work Vitis HLS needs to be called as: vitis_hls this_script.tcl [args]
+set arglen [llength $argv]
+set arg_idx 0
+
+#set default parameters
+# mode param | 0: all steps | 1: just syn & export
+set mode 0 
+
+
+#mode
+if { $arg_idx < $arglen } {
+  set mode [lindex $argv $arg_idx]
+  incr arg_idx
+}
+puts "Mode : $mode" 
+
 open_project TSG_coder.hls_prj
 set_top TSG_coder
 add_files src/TSG_coder.cpp
@@ -10,7 +27,11 @@ open_solution "solution1" -flow_target vivado
 set_part {xc7z020clg484-1}
 create_clock -period 10 -name default
 #source "./src/directives.tcl"
-csim_design -clean
+if { $mode == 0 } {
+  csim_design -clean
+}
 csynth_design
-cosim_design -enable_dataflow_profiling -trace_level all
-# export_design -flow syn -format ip_catalog
+if { $mode == 0 } {
+  cosim_design -enable_dataflow_profiling -trace_level all
+}
+export_design -format ip_catalog
