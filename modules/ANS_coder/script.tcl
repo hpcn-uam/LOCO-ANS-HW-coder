c0f8e5c1ff5c466bb605edb0053c4c8caa304437
tobi <tobias.alonso@estudiante.uam.es>
2021-07-08 16:13:08 +0200

    [ANS_coder] Add makefile and no-sim HLS script. Update subsymbol_gen module name

--- modules/ANS_coder/script.tcl
+++ modules/ANS_coder/script.tcl
@@ -1,5 +1,24 @@
-open_project ANS_coder.hls_prj
-set_top ANS_coder_top
+
+# For arg parsing to work Vitis HLS needs to be called as: vitis_hls this_script.tcl [args]
+set arglen [llength $argv]
+set arg_idx 0
+
+#set default parameters
+# mode param | 0: all steps | 1: just syn & export
+set mode 0 
+
+
+#mode
+if { $arg_idx < $arglen } {
+  set mode [lindex $argv $arg_idx]
+  incr arg_idx
+}
+puts "Mode : $mode" 
+
+set module ANS_coder
+
+open_project "${module}.hls_prj"
+set_top "${module}"
 add_files src/ANS_coder.cpp -cflags "-DANS_CODER_TOP"
 add_files -tb src/test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 add_files -tb ../subsym_gen/src/subsym_gen.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
@@ -9,8 +28,11 @@ set_part {xc7z020-clg484-1}
 create_clock -period 10 -name default
 config_export -format ip_catalog -rtl verilog
 # source "./directives.tcl"
-csim_design -clean
+if { $mode == 0 } {
+  csim_design -clean
+}
 csynth_design
-cosim_design -enable_dataflow_profiling -trace_level all
-export_design -rtl verilog -format ip_catalog
-# export_design -flow impl -rtl verilog -format ip_catalog
+if { $mode == 0 } {
+  cosim_design -enable_dataflow_profiling -trace_level all
+}
+export_design -format ip_catalog
