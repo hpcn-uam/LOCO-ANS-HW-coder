689295d94d99ec791669a31195304e443a406931
tobi <tobias.alonso@estudiante.uam.es>
2021-08-09 10:06:10 +0200

    [subsym_gen] Add Obj. Oriented version of the subsym generator in order to be able to instantiate multiple coders (the functional version uses static variables). Implement test module subsymbol_gen_double_lane, passing RTL co-sim

--- modules/subsym_gen/src/subsym_gen.hpp
+++ modules/subsym_gen/src/subsym_gen.hpp
@@ -19,19 +19,62 @@
 #define SUBSYMB_BYPASS 2
 #define SUBSYMB_Z_LAST 3
 
+#define Z_META_STREAM_SIZE (LOG2_Z_SIZE+CARD_BITS+ANS_SYMB_BITS+ 1+ 2*Z_SIZE+THETA_SIZE+1)
+
 struct subsymb_t{
     ap_uint<SUBSYMB_SIZE> subsymb;
     ap_uint<2> type;
     ap_uint<SUBSYMB_INFO_SIZE> info; // number of bits subsymb contains (for bypass type) // param if z or y
     ap_uint<1> end_of_block;
 
-
-  };
+  friend bool operator==(const subsymb_t& l,const subsymb_t& r){
+    return (l.subsymb == r.subsymb )&&
+            (l.type == r.type )&&
+            (l.info == r.info )&&
+            (l.end_of_block == r.end_of_block );
+  }
+};
 
 void subsymbol_gen(
   stream<coder_interf_t> &in,
   stream<subsymb_t> &symbol_stream
   );
 
+
+class SubSymbolGenerator {
+public:
+  ap_uint<Z_SIZE> module_reminder;
+  ap_uint<1> input_select;
+private:
+  ap_uint<ANS_SYMB_BITS> ans_symb;
+  ap_uint<CARD_BITS> encoder_cardinality;
+  ap_uint<THETA_SIZE> theta_id;
+  ap_uint<1> end_of_block;
+  ap_uint<1> escape_symbol_flag;
+
+  //serialize_symbols
+public:
+  SubSymbolGenerator():input_select(0),module_reminder(0)
+  {};
+
+  void z_decompose_post(
+    stream<ap_uint <Z_META_STREAM_SIZE> > &z_stream_with_meta,
+    stream<subsymb_t> &z_decomposed);
+
+  void serialize_symbols(
+    stream<ap_uint <Y_SIZE+P_SIZE> > &y_stream,
+    stream<subsymb_t > &z_decomposed,
+    stream<subsymb_t> &symbol_stream);
+
+  void transform(
+    stream<coder_interf_t> &in,
+    stream<subsymb_t> &symbol_stream);
+};
+
+void subsymbol_gen_double_lane(
+  stream<coder_interf_t> &in_0,
+  stream<subsymb_t> &symbol_stream_0,
+  stream<coder_interf_t> &in_1,
+  stream<subsymb_t> &symbol_stream_1);
 
 #endif // SUBSYMB_GEN_HPP
