61b3f38619f033c38fee4164b85c4f9984f0c3be
tobias-alonso <tobias.alonso@uam.es>
2021-08-11 23:48:39 +0200

    [Modules] Target higher freqs if they still have room for timing improvement

--- modules/DMA_engines/idma_script.tcl
+++ modules/DMA_engines/idma_script.tcl
@@ -7,14 +7,14 @@
 #  @Email         : tobi.alonso@gmail.com
 #  @Created On    : 2021-07-02 11:10:04
 #  @Description   :
-# 
-# 
-# 
-#  Last Modified : 2021-07-27 11:39:59 
-# 
-#  Revision      : 
-# 
-#  
+#
+#
+#
+#  Last Modified : 2021-07-27 11:39:59
+#
+#  Revision      :
+#
+#
 # ==================================================================================================
 
 # For arg parsing to work Vitis HLS needs to be called as: vitis_hls this_script.tcl [args]
@@ -23,7 +23,7 @@ set arg_idx 0
 
 #set default parameters
 # mode param | 0: all steps | 1: just syn & export
-set mode 0 
+set mode 0
 
 
 #mode
@@ -31,7 +31,7 @@ if { $arg_idx < $arglen } {
   set mode [lindex $argv $arg_idx]
   incr arg_idx
 }
-puts "Mode : $mode" 
+puts "Mode : $mode"
 
 
 open_project idma.hls_prj
@@ -50,4 +50,4 @@ csynth_design
 if { $mode == 0 } {
   cosim_design -enable_dataflow_profiling -trace_level all
 }
-export_design -format ip_catalog
+export_design -format ip_catalog -rtl verilog
